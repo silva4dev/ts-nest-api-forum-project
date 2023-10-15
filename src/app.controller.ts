53707227857dc8a2536076af84fecb89da7f44c7
silva4dev <web4dev.lucas@outlook.com>
2023-10-15 16:18:31 -0300

    chore: auto fix with eslint and prettier

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller('/api')
 export class AppController {
@@ -7,9 +7,8 @@ export class AppController {
 
   @Get('/hello')
   index(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
-
 
   @Post('/hello')
   store(): string {
