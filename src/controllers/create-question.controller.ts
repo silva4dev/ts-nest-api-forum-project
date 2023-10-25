9e92bc17d5f02a9c1fad1ddf1d5887706a03aa24
silva4dev <web4dev.lucas@outlook.com>
2023-10-25 11:55:02 -0300

    chore: add current user decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common'
-import type { Request } from 'express'
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { TokenPayload } from 'src/auth/jwt.strategy'
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   @Post()
-  async handle(@Req() request: Request) {
-    console.log(request.user)
+  async handle(@CurrentUser() user: TokenPayload) {
+    console.log(user)
 
     return 'ok'
   }
