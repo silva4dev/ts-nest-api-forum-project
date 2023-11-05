f8ed3dffe633d4385180b6dc5e9bdba874468d9c
silva4dev <web4dev.lucas@outlook.com>
2023-11-05 06:01:51 -0300

    chore: move to infra folder

---
+++ src/infra/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './http/controllers/create-account.controller'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './http/controllers/authenticate.controller'
+import { CreateQuestionController } from './http/controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-questions.controller'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [
+    AuthenticateController,
+    CreateAccountController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+  providers: [PrismaService],
+})
+export class AppModule {}
